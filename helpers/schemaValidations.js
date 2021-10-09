59705c03ed751f2480b6560bd7fbd6fa4adc43a2
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-10-09 13:39:57 +0330

    changed the snakeCase into camelCase

--- helpers/schemaValidations.js
+++ helpers/schemaValidations.js
@@ -2,13 +2,13 @@ const Joi = require("joi");
 
 exports.movementSchemaValidation = () => {
   return Joi.object({
-    movement_name: Joi.string().min(4).required(),
-    movement_display_name: Joi.string().min(4).required(),
-    video_url: Joi.string().required(),
-    video_start_time: Joi.number().required(),
-    video_end_time: Joi.number().required(),
-    video_source: Joi.string().required(),
-    equipment_list: Joi.array().items(Joi.string()).required(),
-    workout_category_list: Joi.array().items(Joi.string()).required(),
+    movementName: Joi.string().min(4).required(),
+    movementDisplayName: Joi.string().min(4).required(),
+    videoUrl: Joi.string().required(),
+    videoStartTime: Joi.number().required(),
+    videoEndTime: Joi.number().required(),
+    videoSource: Joi.string().required(),
+    equipmentList: Joi.array().items(Joi.string()).required(),
+    workoutCategoryList: Joi.array().items(Joi.string()).required(),
   });
 };
