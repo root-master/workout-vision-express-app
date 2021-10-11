48974997f822f2f9a16e75465fd60e60ace35afb
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-10-11 16:07:19 +0330

    added timestamps for movements

--- schema/Movement.schema.js
+++ schema/Movement.schema.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
 
-const MovementSchema = new Schema({
-  movementName: String,
-  movementDisplayName: String,
-  equipmentList: Array(String),
-  videoUrl: String,
-  videoSource: String,
-  videoStartTime: Number,
-  videoEndTime: Number,
-  workoutCategoryList: Array(String),
-});
+const MovementSchema = new Schema(
+  {
+    movementName: String,
+    movementDisplayName: String,
+    equipmentList: Array(String),
+    videoUrl: String,
+    videoSource: String,
+    videoStartTime: Number,
+    videoEndTime: Number,
+    workoutCategoryList: Array(String),
+  },
+  { timestamps: true }
+);
 
-const Movement = mongoose.model("movement", MovementSchema);
+const Movement = model("movement", MovementSchema);
 
 module.exports = Movement;
